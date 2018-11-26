aa5c616cc358f916767a72a59e07b9bb7818907f
Andrés <andresnicolastonello@gmail.com>
2018-11-26 02:39:43 -0300

    Ok register worked. Now polyfill url didn't work.

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@ limitations under the License.
 
 // This polyfill provides Cache.add(), Cache.addAll(), and CacheStorage.match(),
 // which are not implemented in Chrome 40.
-importScripts('/js/dependencies/cache-polyfill.js');
+importScripts('./js/dependencies/cache-polyfill.js');
 
 // While overkill for this specific sample in which there is only one cache,
 // this is one best practice that can be followed in general to keep track of
